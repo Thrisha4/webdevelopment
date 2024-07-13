6df9624c10a2fd0c49b2fc408fb7d65cda3ea626
Thrisha4 <143954090+Thrisha4@users.noreply.github.com>
2024-07-13 17:04:03 +0530

    Add files via upload

---
+++ portfolio/script.js
@@ -0,0 +1,10 @@
+var typed = new Typed((".skills"),{
+    strings : ["Frontend developer","Backend developer","programmer"],
+    typeSpeed : 100,
+    backSpeed : 100,
+    backDelay : 1000,
+    loop : true
+})
+document.getElementById('downloadBtn').addEventListener('click', function() {
+    window.location.href = 'Resume.jpg';
+  });
